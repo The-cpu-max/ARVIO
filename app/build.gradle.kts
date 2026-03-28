5818dc7cb54e7131dd6926d8ca35cd4a0e2d2636
Arvin <arvin@arflix.local>
2026-03-29 00:08:15 +0100

    release: bump version to 1.9.5 (224)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // Lower minSdk to maximize compatibility and avoid "There was a problem parsing the package".
         minSdk = 21
         targetSdk = 35
-        versionCode = 223
-        versionName = "1.9.4"
+        versionCode = 224
+        versionName = "1.9.5"
         buildConfigField("String", "GITHUB_OWNER", "\"ProdigyV21\"")
         buildConfigField("String", "GITHUB_REPO", "\"ARVIO\"")
 
